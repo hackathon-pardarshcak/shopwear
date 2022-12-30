3f066ca79e8c09d2f31124383f47783160f2bc48
Dharti <dhartic.itpath@gmail.com>
2022-12-30 11:34:55 +0530

    Done:   - common color file created

--- lib/UI/CustomWidgets/custom_appbar.dart
+++ lib/UI/CustomWidgets/custom_appbar.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../Utils/app_colors.dart';
+
 PreferredSizeWidget commonAppBar(appTitle,searchVisibility) {
   return AppBar(
     title: Text(
       appTitle,
       style: TextStyle(
-        color: Color(0xFF000000),
+        color: AppColors.bgBlack,
       ),
     ),
     centerTitle: true,
-    leading: IconButton(onPressed: (){}, icon: Icon(Icons.menu),color: Color(0xFF000000)),
+    leading: IconButton(onPressed: (){}, icon: Icon(Icons.menu),color: AppColors.bgBlack),
     actions: [
-      Visibility(visible: searchVisibility,child: IconButton(onPressed: (){}, icon: Icon(Icons.search),color: Color(0xFF000000))),
+      Visibility(visible: searchVisibility,child: IconButton(onPressed: (){}, icon: Icon(Icons.search),color: AppColors.bgBlack)),
     ],
-    backgroundColor: Color(0xFFFFFFFF),
+    backgroundColor: AppColors.bgWhite,
   );
 }
